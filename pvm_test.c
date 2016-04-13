a478bbe579c87abf1f4f4d41bfd0cef33688b378
Oscar Saleta Reig <oscar.saleta@gmail.com>
2016-04-13 11:39:59 +0200

    Added license boilplate

--- pvm_test.c
+++ pvm_test.c
@@ -1,4 +1,23 @@
-/*! \mainpage PVM general parallellizer for antz
+/* Job parallelizer in PVM for SPMD executions in antz computing server
+ * URL: https://github.com/oscarsaleta/PVMantz
+ *
+ * Copyright (C) 2016  Dept. Matematiques UAB
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+/*! \mainpage PVM general parallelizer for antz
  * \author Oscar Saleta Reig
  */
 
