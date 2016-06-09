94641a7d8194f3afe936878d7214feab0dec993a
Oscar Saleta Reig <oscar.saleta@gmail.com>
2016-06-09 11:57:57 +0200

    Updated version name

--- antz_lib.h
+++ antz_lib.h
@@ -3,7 +3,7 @@
  * \author Oscar Saleta Reig
  */
 
-#define VERSION "2.0.0"
+#define VERSION "2.0.1"
 #define PVM_ENCODING PvmDataRaw ///< Little Endian encoding
 #define MAX_NODE_LENGTH 6 ///< Max length of node names (a0X)
 #define FNAME_SIZE 150 ///< Max length of filename (including path)
