06a402432fe4baac16aef0f81023774452836f40
Oscar Saleta Reig <oscar.saleta@gmail.com>
2017-04-26 13:29:25 +0200

    remove aux octave scripts at execution end

--- src/PBala.c
+++ src/PBala.c
@@ -607,8 +607,8 @@ int main(int argc, char *argv[])
                 inp_programFile, inp_programFile);
         ignored = system(aux_str);
     }
-    // remove tmp pari/sage programs (if created)
-    if (task_type == 3 || task_type == 4) {
+    // remove tmp pari/sage/octave programs (if created)
+    if (task_type == 3 || task_type == 4 || task_type == 5) {
         DIR *dir;
         struct dirent *ent;
         dir = opendir(out_dir);
