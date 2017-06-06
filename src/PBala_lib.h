1d259328f497c88c694dca386a87e59c4eca5e40
Oscar Saleta Reig <oscar.saleta@gmail.com>
2017-06-06 14:25:59 +0200

    add maplesinglecpu and time substract functions and doxygen docs

--- src/PBala_lib.h
+++ src/PBala_lib.h
@@ -23,6 +23,7 @@
  * \author Oscar Saleta Reig
  */
 
+#include <sys/time.h>
 #include <sys/resource.h>
 
 #define PVM_ENCODING PvmDataRaw ///< Little Endian encoding
@@ -34,21 +35,170 @@
 #define MSG_RESULT 3 ///< Flag that indicates that message contains results
 #define MSG_STOP 4   ///< Flag for stopping a task
 
-int getLineCount(char *inp_dataFile);
+/**
+ * Prepare Maple scripts for single CPU executions
+ *
+ * @param fname name of input file
+ * @return      0 if successful
+ */
+int mapleSingleCPU(char *fname);
+/**
+ * Count how many lines a textfile has
+ *
+ * @param fname name of file to be counted
+ * @return      int lineCount, -1 if an error occurred
+ */
+int getLineCount(char *fname);
+/**
+ * Parse the node file and return an array of strings containing the names
+ * of the nodes, and an array of ints containing their number of CPUs
+ *
+ * @param nodefile  name of node file
+ * @param nNodes    number of nodes
+ * @param nodes     pointer to array of strings of node names
+ * @param nodeCores pointer to array of ints of CPUs per node
+ * @return          0 if successful, 1 if error opening file, 2 if error reading
+ */
 int parseNodefile(char *nodefile, int nNodes, char ***nodes, int **nodeCores);
+/**
+ * Memory check for tasks. If no guess is given, limit to 25% of max RAM
+ *
+ * @param flag          Tells the function if there is a guess by the user
+ * @param max_task_size If flag!=0 use this as constraint to memory
+ * @return              0 if there is enough memory, 1 if there is not enough
+ *                      memory, -1 if an error occurred
+ */
 int memcheck(int flag, long int max_task_size);
+/**
+ * Subtract the two timespec structs
+ *
+ * @param result struct timespec where the substraction is stored
+ * @param x      struct timespec X
+ * @param y      struct timespec Y
+ * @return       1 if the difference is negative, otherwise 0
+ */
+int timespec_subtract(struct timespec *result, struct timespec *x,
+                     struct timespec *y);
+/**
+ * Print usage information to output file
+ *
+ * @param pid        Proccess identifier (within system)
+ * @param taskNumber Task identifier (within our program)
+ * @param usage      Struct that contains all the resource usage information
+ * @param out_dir    Output file name
+ * @return           0 if successful
+ */
 int prtusage(int pid, int taskNumber, char *out_dir, struct rusage usage);
+/**
+ * Creates an auxiliary program for PARI execution
+ *
+ * @param taskId      Task number
+ * @param args        Arguments string separated by commas
+ * @param programfile path to PARI script
+ * @param directory   path to results directory
+ * @return            0 if successful, -1 if error occurred
+ */
 int parifile(int taskId, char *args, char *programfile, char *directory);
+/**
+ * Creates an auxiliary program for Sage execution
+ *
+ * @param taskId      Task number
+ * @param args        Arguments string separated by commas
+ * @param programfile path to script
+ * @param directory   path to results directory
+ * @return            0 if successful
+ */
 int sagefile(int taskId, char *args, char *programfile, char *directory);
+/**
+ * Creates an auxiliary program for Octave execution
+ *
+ * @param taskId      Task number
+ * @param args        Arguments string separated by commas
+ * @param programfile Path to script
+ * @param directory   Path to the results directory
+ * @return            0 if successful
+ */
 int octavefile(int taskId, char *args, char *programfile, char *directory);
+/**
+ * Informs that a process has been killed or stopped
+ * this function runs instead of prtusage()
+ *
+ * @param pid        Proccess identifier (within system)
+ * @param taskNumber Task identifier (within our program)
+ * @param out_dir    Output file name
+ * @return           0 if successful, -1 if file error
+ */
 int prterror(int pid, int taskNumber, char *out_dir, double time);
+/**
+ * Kill every PBala related process from every antz node
+ * @return 0
+ */
 int killPBala(void);
-
-int mapleProcess(int taskNumber, char *inputFile, char *arguments, char *customPath);
-int cProcess(int taskNumber, char *inputFile, char *arguments, char *customPath);
-int pythonProcess(int taskNumber, char *inputFile, char *arguments, char *customPath);
+/**
+ * Generate system command for Maple execution and run it
+ *
+ * @param  taskNumber task number
+ * @param  inputFile  name of input file
+ * @param  arguments  string of comma separated arguments
+ * @param  customPath custom path to executable (if applies)
+ * @return            output of execvp of the generated string
+ */
+int mapleProcess(int taskNumber, char *inputFile, char *arguments,
+                 char *customPath);
+/**
+ * Generate system command for C execution and run it
+ *
+ * @param  taskNumber task number
+ * @param  inputFile  name of input file
+ * @param  arguments  string of comma separated arguments
+ * @param  customPath custom path to executable (if applies)
+ * @return            output of execvp of the generated string
+ */
+int cProcess(int taskNumber, char *inputFile, char *arguments,
+             char *customPath);
+/**
+ * Generate system command for Python execution and run it
+ *
+ * @param  taskNumber task number
+ * @param  inputFile  name of input file
+ * @param  arguments  string of comma separated arguments
+ * @param  customPath custom path to executable (if applies)
+ * @return            output of execvp of the generated string
+ */
+int pythonProcess(int taskNumber, char *inputFile, char *arguments,
+                  char *customPath);
+/**
+ * Generate system command for PARI/GP execution and run it
+ *
+ * It also creates the auxiliary PARI scripts
+ *
+ * @param  taskNumber task number
+ * @param  outdir     output directory name
+ * @param  customPath custom path to executable (if applies)
+ * @return            output of execvp of the generated string
+ */
 int pariProcess(int taskNumber, char *outdir, char *customPath);
+/**
+ * Generate system command for Sage execution and run it
+ *
+ * It also creates the auxiliary Sage scripts
+ *
+ * @param  taskNumber task number
+ * @param  outdir     output directory name
+ * @param  customPath custom path to executable (if applies)
+ * @return            output of execvp of the generated string
+ */
 int sageProcess(int taskNumber, char *outdir, char *customPath);
+/**
+ * Generate system command for Octave execution and run it
+ *
+ * It also creates the auxiliary Octave scripts
+ *
+ * @param  taskNumber task number
+ * @param  outdir     output directory name
+ * @param  customPath custom path to executable (if applies)
+ * @return            output of execvp of the generated string
+ */
 int octaveProcess(int taskNumber, char *outdir, char *customPath);
 
 #endif /* PBALA_LIB_H */
